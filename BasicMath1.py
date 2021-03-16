8289b441db7d2669a967b3faf15b9c70a352da4b
PedroTomazeti23 <80048556+PedroTomazeti23@users.noreply.github.com>
2021-03-16 11:30:30 -0300

    Add files via upload

---
+++ BasicMath1.py
@@ -0,0 +1,3 @@
+n = int(input('Insert desired number: '))
+print(f'Analyzing the entire value {n}, your predecessor is {n - 1} and your successor is {n + 1} !')
+print(f'Your double is {n * 2}, your triple is {n * 3} and your square root is {pow(n, 1 / 2)} !')
---
+++ angles(sin,cos,tan).py
@@ -0,0 +1,5 @@
+import math
+angle = float(input('Insert the desired angle: '))
+print('The sine of {:.2f}° in radian is {:.2f}'.format(angle, math.sin(math.radians(angle))))
+print('The cosine of {:.2f}° in radian is {:.2f}'.format(angle, math.cos(math.radians(angle))))
+print('The tangent of {:.2f}° in radian is {:.2f}'.format(angle, math.tan(math.radians(angle))))
