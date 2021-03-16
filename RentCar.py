cfaed98b0c5d836c9e07d23c7c1ad7e1ec769000
PedroTomazeti23 <80048556+PedroTomazeti23@users.noreply.github.com>
2021-03-16 12:03:10 -0300

    Add files via upload

--- RentCar.py
+++ RentCar.py
@@ -1,19 +1,15 @@
 days = int(input('How many days rented? '))
 distance = float(input('How many kilometer traveled? '))
-rateDays = int(input('What flat fee in your local currency is charged for day: '))
+rateDays = float(input('What flat fee in your local currency is charged for day: '))
 rateDistance = float(input('What flat fee in your local currency is charged for distance? '))
 print('Enter your local currency: ')
 print('1 - American dollar($); \n2 - Euro(€); \n3 - Reais(R$);')
 coin = int(input('Which option? '))
 if coin == 1:
-    coinChoice = '$'
-    print('The total amount to be paid is {}{:.2f}'.format(coinChoice, days * rateDays + distance * rateDistance))
+    print(f'The total amount to be paid is ${days * rateDays + distance * rateDistance:.2f}')
 elif coin == 2:
-    coinChoice = '€'
-    print('The total amount to be paid is {}{:.2f}'.format(coinChoice, days * rateDays + distance * rateDistance))
+    print(f'The total amount to be paid is €{days * rateDays + distance * rateDistance:.2f}')
 elif coin == 3:
-    coinChoice = 'R$'
-    print('The total amount to be paid is {}{:.2f}'.format(coinChoice, days * rateDays + distance * rateDistance))
+    print(f'The total amount to be paid is R${days * rateDays + distance * rateDistance:.2f}')
 else:
-    coinChoice = None
     print('Invalid value!')
